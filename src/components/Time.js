670d9543146e1a8863f56f2653adcf93ff76dfa6
Aidana777 <aidananiazalieva27@gmail.com>
2023-03-19 16:51:01 +0600

    timer

---
+++ src/components/Time.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+
+function Time() {
+    const [clockState, setClockState] = useState();
+
+    useEffect(() => {
+        setInterval(() => {
+            const date = new Date();
+            setClockState(date.toLocaleTimeString())
+        }, 1000)
+    }, [])
+
+    return <div className="clock">{clockState}</div>
+
+}
+
+export default Time
